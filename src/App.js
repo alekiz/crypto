d37df4e809de220590f09dc0ec98786a95104da6
alekiz <116733887+alekiz@users.noreply.github.com>
2025-03-13 12:07:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {Browserouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 import Login from "./components/SignIn.jsx";
 import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from "./components/index.js";
